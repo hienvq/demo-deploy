347f9622595fc8aac1518ee1a3c8b7309c5eccd7
Hien Vu Quang (hienvq) <hienvq@curama.vn>
2023-08-31 14:52:10 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,40 +4,43 @@ import LoginPage from "../pages/LoginPage";
 import ProductPage from "../pages/ProductPage";
 import UserPage from "../pages/UserPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LoginPage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/admin",
-    children: [
-      {
-        path: "",
-        element: <h1>abc</h1>,
-      },
-      {
-        path: "product",
-        element: <PrivateComponent component={ProductPage} />,
-      },
-      {
-        path: "product/details/:id",
-        element: <PrivateComponent component={UserPage} />,
-      },
-      {
-        path: "user",
-        element: <PrivateComponent component={UserPage} />,
-      },
-      {
-        path: "category",
-        element: <h1>category</h1>,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <LoginPage />,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/admin",
+      children: [
+        {
+          path: "",
+          element: <h1>abc</h1>,
+        },
+        {
+          path: "product",
+          element: <PrivateComponent component={ProductPage} />,
+        },
+        {
+          path: "product/details/:id",
+          element: <PrivateComponent component={UserPage} />,
+        },
+        {
+          path: "user",
+          element: <PrivateComponent component={UserPage} />,
+        },
+        {
+          path: "category",
+          element: <h1>category</h1>,
+        },
+      ],
+    },
+  ],
+  { basename: "web2" }
+);
 
 export default router;
